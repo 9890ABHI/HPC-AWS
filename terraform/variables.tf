78c73d80883dfd7f0fd2cad3fdc7f0269d6c4033
9890ABHI <abhipawar7588@gmail.com>
2026-02-27 17:28:38 +0530

    default added in region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,6 +17,7 @@ variable "key_name" {
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "aws_availability_zone" {
